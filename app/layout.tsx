c508d95610a796ed4cc9e78029362e3c4d1be7d0
regenerik <regenerik@gmail.com>
2026-08-14 02:48:07 -0300

    Add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { HydrationMarker } from "@/components/HydrationMarker";
 import { LanguageProvider } from "@/components/LanguageProvider";
 import { UIScaleProvider } from "@/components/UIScaleProvider";
 import "./globals.css";
+
+const googleTagManagerId = "GTM-WL8B6CWS";
+
+const googleTagManagerScript = `
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${googleTagManagerId}');
+`;
 
 const hydrationRecoveryScript = `
   (() => {
@@ -38,7 +49,21 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="es">
+      <Script
+        id="google-tag-manager"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: googleTagManagerScript }}
+      />
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <script dangerouslySetInnerHTML={{ __html: hydrationRecoveryScript }} />
         <HydrationMarker />
         <LanguageProvider>
